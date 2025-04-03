29c028b7648dd8dd9d6d1fd336f01cd14152cd74
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-03 19:16:17 -0300

    feat: added my two components Heading and Container in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui beatae
-            nemo officiis facere repudiandae autem, optio quae voluptatibus sunt
-            veritatis eum nam ducimus ipsum amet! Molestiae et optio amet alias.
-          </section>
-        </div>
-      </div>
+      <Container>
+        <Heading>Este é meu componente</Heading>
+      </Container>
     </>
   );
 }
