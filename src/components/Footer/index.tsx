b5c7edef84b1e0a55be7b3652ec85d6bed11fa02
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-04 22:22:46 -0300

    feat: created Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+
+export function Footer() {
+  return (
+    <div className={styles.footerContainer}>
+      <a href='#'>Entenda o conceito do Pomodoro</a>
+      <a href='#'>Chronos Pomodoro &copy; Feito com 💚.</a>
+    </div>
+  );
+}
