a43f4d6912abe84d1b97c1c70ae8e6c55ad29a5e
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-06 19:31:34 -0300

    feat: refactored css for module in NotFound component

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,12 +1,15 @@
+import styles from './sytles.module.css';
+
 import { PlayCircleIcon } from 'lucide-react';
+
 import { Button } from '../Button';
 import { Cycles } from '../Cycles';
 import { Input } from '../Input';
 
 export function MainForm() {
   return (
-    <form className='formContainer' action=''>
-      <div className='formRow'>
+    <form className={styles.formContainer} action=''>
+      <div className={styles.formRow}>
         <Input
           id='input'
           label='Task'
@@ -15,15 +18,15 @@ export function MainForm() {
         />
       </div>
 
-      <div className='formRow'>
+      <div className={styles.formRow}>
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
 
-      <div className='formRow'>
+      <div className={styles.formRow}>
         <Cycles />
       </div>
 
-      <div className='formRow'>
+      <div className={styles.formRow}>
         <Button icon={<PlayCircleIcon />} color='green' />
         {/* <Button icon={<StopCircleIcon />} color='red' /> */}
       </div>
