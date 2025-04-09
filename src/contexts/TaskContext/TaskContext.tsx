dbfa733aa86ddb392b5cdc2799a360c406255041
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-08 21:48:05 -0300

    chore: added comments for context component

--- src/contexts/TaskContext/TaskContext.tsx
+++ src/contexts/TaskContext/TaskContext.tsx
@@ -2,12 +2,13 @@ import { createContext } from 'react';
 import { TaskStateModel } from '../../models/TaskStateModel';
 import { initialState } from './initialTaskState';
 
-// Tipagem de state e setState para serem recebidos no provider do context.
+// Tipagem do Taskcontext onde state e setState é objeto dentro de initialContextState passados para o provider do context.
 type TaskContextTypes = {
   state: TaskStateModel;
   setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
 };
 
+// Valores iniciais do TaskContext
 const initialContextValue = {
   state: initialState,
   setState: () => {},
