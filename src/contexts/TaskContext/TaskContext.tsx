d2a40a5ba89aa77e3a3c4de5d24235217be36356
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-12 22:11:15 -0300

    feat: refactored context for receive dispatch of reducer

--- src/contexts/TaskContext/TaskContext.tsx
+++ src/contexts/TaskContext/TaskContext.tsx
@@ -1,17 +1,18 @@
 import { createContext } from 'react';
 import { TaskStateModel } from '../../models/TaskStateModel';
-import { initialState } from './initialTaskState';
+import { initialTaskState } from './initialTaskState';
+import { TaskActionModel } from './taskAction';
 
 // Tipagem do Taskcontext onde state e setState é objeto dentro de initialContextState passados para o provider do context.
 type TaskContextTypes = {
   state: TaskStateModel;
-  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+  dispatch: React.Dispatch<TaskActionModel>;
 };
 
 // Valores iniciais do TaskContext
 const initialContextValue = {
-  state: initialState,
-  setState: () => {},
+  state: initialTaskState,
+  dispatch: () => {},
 };
 
 export const TaskContext = createContext<TaskContextTypes>(initialContextValue);
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from 'react';
-import { initialState } from './initialTaskState';
+import { useEffect, useReducer } from 'react';
+
+// Context and Reducer
+import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
+import { taskReducer } from './taskReducer';
 
 type TaskContextProviderTypes = {
   children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderTypes) {
-  const [state, setState] = useState(initialState);
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
   // Emitir resultado do estado na tela sempre que o state mudar.
   useEffect(() => {
@@ -15,7 +18,7 @@ export function TaskContextProvider({ children }: TaskContextProviderTypes) {
   }, [state]);
 
   return (
-    <TaskContext.Provider value={{ state, setState }}>
+    <TaskContext.Provider value={{ state, dispatch }}>
       {children}
     </TaskContext.Provider>
   );
