730bfb70cdb36fc987b370c762e4e0fd97aa085e
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-20 19:18:09 -0300

    feat: separete component BrowserRouter to other component usefull adapter pattern and add scroll to top

---
+++ src/routers/MainRouter/index.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
+
+import { Home } from '../../pages/Home';
+import { NotFound } from '../../pages/NotFound';
+import { About } from '../../pages/About';
+import { useEffect } from 'react';
+
+// 'pathname' não pode ser capturado antes de ser iniciado um Router, uso dentro de um componente que está dentro do Router.
+// Toda vez que o 'path'/url mudar é executado o scroll to top.
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname]);
+  return null; // O componente existe, mas não retorna nada.
+}
+
+export function MainRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about-pomodoro/' element={<About />} />
+
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <ScrollToTop />{' '}
+      {/* Passando o 'pathname' pra dentro do componente que contém Router.  */}
+    </BrowserRouter>
+  );
+}
