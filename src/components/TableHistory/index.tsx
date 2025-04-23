765d63f85bc5242b4043c26eb645379f658aa5cf
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-22 22:05:28 -0300

    feat: sorting array tasks on click user in table title

--- src/components/TableHistory/index.tsx
+++ src/components/TableHistory/index.tsx
@@ -1,27 +1,75 @@
+import { useState } from 'react';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 import { formatDate } from '../../utils/formatDate';
 import { getTaskStatus } from '../../utils/getTaskStatus';
+import { sortTasks, SortTasksOptions } from '../../utils/sortTasks';
 
 import styles from './styles.module.css';
 
 export function TableHistory() {
   const { state } = useTaskContext();
+  const [storedTaskChoise, setStoredTaskChoise] = useState<SortTasksOptions>(
+    () => {
+      return {
+        tasks: sortTasks({ tasks: state.tasks }),
+        direction: 'desc',
+        field: 'startDate',
+      };
+    },
+  );
+
+  function handleStoredTaskChoiseClick({
+    field,
+  }: Pick<SortTasksOptions, 'field'>) {
+    const setDirection = storedTaskChoise.direction === 'asc' ? 'desc' : 'asc';
+
+    setStoredTaskChoise({
+      tasks: sortTasks({
+        direction: setDirection,
+        tasks: storedTaskChoise.tasks,
+        field,
+      }),
+      direction: setDirection,
+      field,
+    });
+  }
 
   return (
     <section className={styles.container}>
       <table>
         <thead>
           <tr>
-            <th>Tarefa</th>
-            <th>Tempo</th>
-            <th>Data</th>
-            <th>Status</th>
-            <th>Tipo</th>
+            <th
+              onClick={() => handleStoredTaskChoiseClick({ field: 'name' })}
+              className={styles.thSort}
+            >
+              Tarefa
+            </th>
+            <th
+              onClick={() => handleStoredTaskChoiseClick({ field: 'duration' })}
+              className={styles.thSort}
+            >
+              Tempo
+            </th>
+            <th
+              onClick={() =>
+                handleStoredTaskChoiseClick({ field: 'startDate' })
+              }
+              className={styles.thSort}
+            >
+              Data
+            </th>
+            <th onClick={() => handleStoredTaskChoiseClick({ field: 'name' })}>
+              Status
+            </th>
+            <th onClick={() => handleStoredTaskChoiseClick({ field: 'name' })}>
+              Tipo
+            </th>
           </tr>
         </thead>
         <tbody>
-          {state.tasks.map(task => {
+          {storedTaskChoise.tasks.map(task => {
             const taskType = {
               workCycle: 'Trabalho',
               breakShortCycle: 'Descanço Curto',
